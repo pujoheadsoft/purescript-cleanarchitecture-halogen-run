7e430786bfb3b6da36dcce66594889ce8f31ce57
Pujo <pujoheadsoft@gmail.com>
2023-05-23 07:46:07 +0900

    remove comment

--- src/Usecases/DisplayCompletedTodos.purs
+++ src/Usecases/DisplayCompletedTodos.purs
@@ -12,28 +12,6 @@ import Type.Row (type (+))
 import Usecases.TodoOutputPort (TODO_OUTPUT_PORT, setTodos, setError)
 import Usecases.TodoPort (TODO_PORT, findTodos)
 
-{-
-  Run (AFF + ...) Unit
-  か
-  MonadAff m => Run (...) (m Unit)
-  のどちらかでないと駄目っぽい
-
-  なぜなら
-  MonadState State m => Run (AFF + ...) (m Unit)
-  だと
-  runBaseAffなどしたときにどうしても
-  Aff (m Unit)
-  になったり
-  MoadAff m => MonadState State m => m (m Unit)
-  になってしまうから
-
-  AFFのパターンでいくなら、MonadStateのmを返せないので、どこかでMonadStateに変換できないといけない。
-  RunのSTATE使って、やれるか？
-
-  MonadAffのパターンのほうがまだできそうか？
-
--}
-
 execute 
   :: UserId
   -> Logics
